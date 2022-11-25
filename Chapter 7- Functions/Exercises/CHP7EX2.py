d02b76a196ac35e5232931f43381134c6cef4eba
Majed Abdulmajid Mirza <ajohnadam14@gmail.com>
2022-11-26 01:39:38 +0400

    Chapter 7 Complete

---
+++ Chapter 7- Functions/Exercises/CHP7EX2.py
@@ -0,0 +1,5 @@
+def favorite_book(title):
+    #Display favorite book message
+    print(f"\n{title} is definitely one of my favorite books.\n")
+
+favorite_book('Solitaire by Alice Oseman')
---
+++ Chapter 7- Functions/Exercises/CHP7EX4.py
@@ -0,0 +1,7 @@
+def make_shirt(size='large', msg='Life isnt fair'):
+    print(f"\nI'm going to make a {size} t-shirt.")
+    print(f'It will say, "{msg}"')
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt('large', 'Never felt this way before!.')
